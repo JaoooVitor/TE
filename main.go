202731b2bc787a32a7693f5b30ddad638cbc355f
JaoooVitor <119083475+JaoooVitor@users.noreply.github.com>
2024-06-30 22:34:41 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"regexp"
 	"strconv"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type Pessoa struct {
@@ -16,127 +12,79 @@ type Pessoa struct {
 	Nome string `json:"nome"`
 }
 
-// docker run --name restApi -e MYSQL_ROOT_PASSWORD=test123 -d mysql:8
-var db *sql.DB
+var pessoas []Pessoa
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch {
-	case r.Method == "GET" && r.URL.Path == "/pessoa":
-		getPessoas(w, r)
-	case r.Method == "GET" && regexp.MustCompile(`^/pessoa/\d+$`).MatchString(r.URL.Path):
-		getPessoaByID(w, r)
-	case r.Method == "POST" && r.URL.Path == "/pessoa":
-		createPessoa(w, r)
-	case r.Method == "DELETE" && regexp.MustCompile(`^/pessoa/\d+$`).MatchString(r.URL.Path):
-		deletePessoaByID(w, r)
-	default:
-		http.Error(w, "Metodo não cadastrado", http.StatusNotFound)
-	}
-}
-
-func getPessoas(w http.ResponseWriter, r *http.Request) {
-
-	rows, err := db.Query("SELECT id, nome FROM pessoas")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var pessoas []Pessoa
-	for rows.Next() {
-		var p Pessoa
-		if err := rows.Scan(&p.ID, &p.Nome); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		pessoas = append(pessoas, p)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
+func getListPessoas(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(pessoas)
-
 }
 
-func getPessoaByID(w http.ResponseWriter, r *http.Request) {
+func getPessoa(w http.ResponseWriter, r *http.Request) {
+	nome := r.URL.Query().Get("nome")
+	idStr := r.URL.Query().Get("id")
 
-	idStr := regexp.MustCompile(`\d+`).FindString(r.URL.Path)
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "ID inválido", http.StatusBadRequest)
-		return
-	}
+	if idStr != "" {
+		id, _ := strconv.Atoi(idStr)
 
-	var p Pessoa
-	err = db.QueryRow("SELECT id, nome FROM pessoas WHERE id = ?", id).Scan(&p.ID, &p.Nome)
-	if err == sql.ErrNoRows {
-		w.Header().Set("Content-Type", "application/json")
+		for _, pessoa := range pessoas {
+			if pessoa.ID == id {
+				json.NewEncoder(w).Encode(pessoa)
+				return
+			}
+		}
 		json.NewEncoder(w).Encode(nil)
 		return
-	} else if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(nil)
+	if nome != "" {
+		for _, pessoa := range pessoas {
+			if pessoa.Nome == nome {
+				json.NewEncoder(w).Encode(pessoa)
+				return
+			}
+		}
+		json.NewEncoder(w).Encode(nil)
+		return
+	}
 }
 
-func createPessoa(w http.ResponseWriter, r *http.Request) {
-
-	var p Pessoa
-	err := json.NewDecoder(r.Body).Decode(&p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	result, err := db.Exec("INSERT INTO pessoas (nome) VALUES (?)", p.Nome)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	id, err := result.LastInsertId()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	p.ID = int(id)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(p)
+func postPessoa(w http.ResponseWriter, r *http.Request) {
+	var pessoa Pessoa
+	_ = json.NewDecoder(r.Body).Decode(&pessoa)
+	pessoa.ID = len(pessoas) + 1
+	pessoas = append(pessoas, pessoa)
+	json.NewEncoder(w).Encode(pessoa)
 }
 
-func deletePessoaByID(w http.ResponseWriter, r *http.Request) {
-
-	idStr := regexp.MustCompile(`\d+`).FindString(r.URL.Path)
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "ID inválido", http.StatusBadRequest)
-		return
+func deletePessoa(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	var novaSlice []Pessoa
+	for _, pessoa := range pessoas {
+		if pessoa.ID != id {
+			novaSlice = append(novaSlice, pessoa)
+		}
 	}
 
-	_, err = db.Exec("DELETE FROM pessoas WHERE id = ?", id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	for i, pessoa := range novaSlice {
+		pessoa.ID = i + 1
+		novaSlice[i] = pessoa
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "Pessoa deletada com sucesso"})
+	pessoas = novaSlice
+	fmt.Fprintf(w, "Pessoa com ID %d foi deletada com sucesso.", id)
 }
 
 func main() {
+	http.HandleFunc("/getListPessoas", getListPessoas)
+	http.HandleFunc("/getPessoa", getPessoa)
+	http.HandleFunc("/postPessoa", postPessoa)
+	http.HandleFunc("/deletePessoa", deletePessoa)
 
-	var err error
-	db, err = sql.Open("mysql", "root:test123@tcp(localhost:3306)/api_db")
-	if err != nil {
-		fmt.Println("Erro ao conectar ao banco de dados:", err)
-		panic(err)
-	}
-	defer db.Close()
+	pessoas = append(pessoas, Pessoa{ID: 1, Nome: "Davi"})
+	pessoas = append(pessoas, Pessoa{ID: 2, Nome: "Karol"})
+	pessoas = append(pessoas, Pessoa{ID: 3, Nome: "João VItor"})
+	pessoas = append(pessoas, Pessoa{ID: 4, Nome: "Edson"})
+	pessoas = append(pessoas, Pessoa{ID: 5, Nome: "Letícia"})
+	pessoas = append(pessoas, Pessoa{ID: 6, Nome: "Jean"})
 
-	http.HandleFunc("/", handler)
-	fmt.Println("Servidor rodando na porta 8080")
-	http.ListenAndServe(":8080", nil)
-
+	_ = http.ListenAndServe(":3333", nil)
 }
